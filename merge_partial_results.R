b349a9830c075631431e2490455f748c5ac81cf0
Alex Pletzer <alexander@gokliya.net>
2022-05-23 23:45:38 +0000

    now merge_partial_results creates flat tables

--- merge_partial_results.R
+++ merge_partial_results.R
@@ -1,4 +1,5 @@
 library(dplyr)
+library(stringr)
 
 args <- commandArgs(trailingOnly=TRUE)
 run_dir <- args[1]
@@ -6,28 +7,49 @@ run_dir <- args[1]
 regex_path <- sprintf("%s/worker_*", run_dir)
 worker_dirs <- Sys.glob(regex_path)
 
+result_path <- sprintf("%s/worker_0000/result.rds", run_dir)
+result <- readRDS(result_path)
+date <- result$simulation$q_1$date
+output_field_names <- names(result$simulation)
+
 parameter_list <- list()
 simulation_list <- list()
 for (worker_dir in worker_dirs) {
+
+	# get the path to this workers's result file
 	result_path <- sprintf("%s/result.rds", worker_dir)
+
+	# read the results
 	result <- readRDS(result_path)
 
-	worker_id <- basename(worker_dir)
+	# extract the worker Id, eg "0001"
+	worker_id <- str_replace(basename(worker_dir), "worker_", "")
 
+	# store the parameter values
 	parameter_list[[worker_id]] <- result$parameter$values
- 	simulation_list[[worker_id]] <- result$simulation
+
+	# store the simulation output values, but without the date, and for each output field
+	for (output_field_name in output_field_names) {
+		simulation_list[[output_field_name]][[worker_id]] <- result$simulation[[output_field_name]] %>% select(-contains("date"))
+	}
 }
 
+# merge the tables
 parameter <- dplyr::bind_rows(parameter_list)
-simulation <- dplyr::bind_cols(simulation_list)
 
-date <- simulation["date...1"]
+simulation <- list()
+for (output_field_name in output_field_names) {
 
-simulation <- simulation %>% select(-contains("date"))
+	simulation[[output_field_name]] <- dplyr::bind_cols(simulation_list[[output_field_name]])
 
-# put back the date
-simulation["date"] <- date
+	# rename the columns run_1, run_2, ... run_n
+	simulation[[output_field_name]] <- rename(simulation[[output_field_name]], run_ = colnames(simulation[[output_field_name]]))
 
+	# add the date back to the simulation table, at column 1
+	simulation[[output_field_name]] <- cbind(date = date, simulation[[output_field_name]])
+}
+
+# save the results to file
 parameter_file <- sprintf("%s/parameter.rds", run_dir)
 print(sprintf("...writing parameter values to %s\n", parameter_file))
 saveRDS(parameter, file = parameter_file)
