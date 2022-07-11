86343e8825d7606907a648e6a6f05fd8ef08e42a
Alex Pletzer <alexander@gokliya.net>
2022-07-11 00:00:11 +0000

    fixed the plotting run1...run10

--- plot_results.R
+++ plot_results.R
@@ -22,8 +22,9 @@ pdf(file = file_name, width = 10, height = 8)
 for (i in 1:nq) {
 	field_name <- sprintf("q_%d", i)
 	data <- sim_data[[field_name]]
-	# got from data, run_1, run_2, ... to date, run, q format
-	data_long <- gather(data, run, q, run_1:run_100, factor_key=TRUE)
+	nms <- names(select(data, contains("run_")))
+	# go from data, run_1, run_2, ... to date, run, q format
+	data_long <- gather(data, run, q, nms[1]:nms[length(nms)], factor_key=TRUE)
 	p <- ggplot(data = data_long, aes(x = date, y = q, colour = run)) + 
 	              geom_line() + theme(legend.position = "none") + ggtitle(field_name)
 	print(p)
