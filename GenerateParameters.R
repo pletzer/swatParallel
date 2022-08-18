c6c6348cbddb5f9e7d92e43212779be2bf0ce04f
Sandy Elliott <elliott@wbn003.cm.cluster>
2022-08-19 11:15:34 +1200

    Sandy tidying up GenerateParameters

--- GenerateParameters.R
+++ GenerateParameters.R
@@ -1,8 +1,7 @@
 # Script to generate parameter table for later use in SwatPlusR
-# Generates a table with a user-specified number of rows (simulations) from a set or parameters
-# with user-specified lower and upper bounds. Rows are generated from Latin
-# hypercube sampling.
-# Names of the parameters are set up in a specifi format used later by SWATPlusR
+# Generates a table with a user-specified number of rows (simulations) from a set of parameters
+# with user-specified lower and upper bounds. Rows are generated from Latin hypercube sampling.
+# Names of the parameters are set up in a specific format used later by SWATPlusR
 
 
 # rm(list = ls()) # clean up
@@ -11,19 +10,13 @@ library(dplyr)
 library(readr)
 library(lhs) # for latin hypercube sampling 
 
-# assume working directory is script location
+# assume working directory is already established
 
 # parameters
 nsimulations = 100
 set.seed(111)
 
-# Specify parameter names and bounds
-
-ParamBounds <- tribble(
-~ParamName,  ~ParamMin,  ~ParamMax,
-"intercept",0.,1.,
-"slope",0.001,1.
-)
+# Specify parameter names and bounds. A row for each parameter
 
 ParamBounds <- tribble(
   ~ParamName,  ~ParamMin,  ~ParamMax,
@@ -41,10 +34,10 @@ maximums <- matrix(ParamBounds$ParamMax,nsimulations,nparams,byrow=TRUE)
 ParamSetMatrix <- minimums + (maximums-minimums) * RanUnitUniform # Matrix with named columns (and eventually rows)
 colnames(ParamSetMatrix) <- ParamBounds$ParamName
 
-ParamSetTribble <- as_tibble(ParamSetMatrix) # set up tibble because this will be expected by SwatPlusR
- # For SwatPlusR, will need to modify tribble names to indicate the file and modification type. 
+ParamSetTibble <- as_tibble(ParamSetMatrix) # set up tibble because this will be expected by SwatPlusR
+ # For SwatPlusR, will need to modify tibble names to indicate the file and modification type. 
 
-saveRDS(ParamSetTribble,'ParameterTable.rds')
+saveRDS(ParamSetTibble,'ParameterTable.rds')
 
 
 
